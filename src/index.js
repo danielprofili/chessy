e973ba1ad3a6d3dbfab2a0aadae0b03c067847f1
dprofili3 <dprofili3@gatech.edu>
2018-10-20 22:26:53 -0400

    Add database

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const config = require('./config')
 const commands = require('./commands')
 const helpCommand = require('./commands/help')
 
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+)};
+
 let bot = require('./bot')
 
 let app = express()
@@ -23,6 +29,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
+app.get('/db', async (req, res) => {
+	try {
+		const client = await pool.connect()
+		const result = await client.query('SELECT * FROM test_table');
+		const results = {' results': (result) ? result.rows : null };
+		res.render('pages/db', results);
+		client.release();
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+})
+	
 
 app.post('/commands/starbot', (req, res) => {
   let payload = req.body
